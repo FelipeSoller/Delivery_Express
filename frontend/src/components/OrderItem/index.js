212b83911340ded6e8dac800e1b24ad3e0724f3a
Felipe Soller <soller@outlook.com>
2020-12-14 01:39:08 -0300

    Ajustes finais

--- frontend/src/components/OrderItem/index.js
+++ frontend/src/components/OrderItem/index.js
@@ -1,23 +1,28 @@
-import React from 'react'
+import React from 'react';
 
 import './styles.css';
 
-function OrderItem({ order }) {     
+function OrderItem({ order }) {   
     
     return (
         <li className="delivery-item">
             <header>
-            <div className="delivery-info">
-                <strong>{order.name}</strong>
-                <span>{order.inputDate}</span>                
-            </div>
+                <div className="delivery-info">
+                    <strong>{order.name}</strong>
+                    <span>{order.inputDate}</span>                
+                </div>
             </header>
 
             <div className="address-info">
-            <p>{order.startPoint}</p>
-            <p>{order.endPoint}</p>
-            <a target="_blank" rel="noopener noreferrer" href={`https://www.google.com.br/maps/dir/${order.startPoint}/${order.endPoint}`}>Ver rotas no Google Maps</a>
-            </div>            
+                <strong>Endereço de Partida</strong>
+                <p>{order.startPoint}</p>
+                <strong>Endereço de Entrega</strong>
+                <p>{order.endPoint}</p>                
+            </div>  
+
+            <div className="buttons">
+                <a target="_blank" rel="noopener noreferrer" href={`https://www.google.com.br/maps/dir/${order.startPoint}/${order.endPoint}`}>Rotas</a>                  
+            </div>                    
         </li>
     )
 }
